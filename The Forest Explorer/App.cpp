35474b6f4fead3f85a3ac4bc015cf3121bffe077
Moeez Abdul <MoeezRajput2000@Gmail.com>
2023-02-12 16:54:37 +0000

    Class Model now has Public nd Private Variables with void function.

--- The Forest Explorer/App.cpp
+++ The Forest Explorer/App.cpp
@@ -9,7 +9,35 @@ using namespace std;
 class Model
 {
 public:
+    Model() : score(0), health(100), inventory() {}
+
+    void add_points(int points) {
+        score += points;
+    }
+
+    void take_damage(int damage) {
+        health -= damage;
+    }
+
+    void add_to_inventory(std::string item) {
+        inventory.push_back(item);
+    }
+
+    int get_score() {
+        return score;
+    }
+
+    int get_health() {
+        return health;
+    }
+
+    std::vector<std::string> get_inventory() {
+        return inventory;
+    }
 private:
+    int score;
+    int health;
+    std::vector<std::string> inventory;
 };
 
 class View

4070d752a4415b11b7c556843a470fb1c37bdc41
Moeez Abdul <MoeezRajput2000@Gmail.com>
2023-02-12 16:55:49 +0000

    Class Controller now has void functions.

--- The Forest Explorer/App.cpp
+++ The Forest Explorer/App.cpp
@@ -49,7 +49,38 @@ private:
 class Controller
 {
 public:
-private:
+    void display_score(int score) {
+        std::cout << "Your current score is: " << score << std::endl;
+    }
+
+    void display_health(int health) {
+        std::cout << "Your current health is: " << health << std::endl;
+    }
+
+    void display_inventory(std::vector<std::string> inventory) {
+        std::cout << "Your current inventory is: ";
+        for (std::string item : inventory) {
+            std::cout << item << " ";
+        }
+        std::cout << std::endl;
+    }
+
+    void display_options() {
+        std::cout << "What would you like to do?" << std::endl;
+        std::cout << "1. Explore the forest" << std::endl;
+        std::cout << "2. View your stats" << std::endl;
+        std::cout << "3. Quit" << std::endl;
+    }
+
+    void display_encounter_result(bool win, std::string item) {
+        if (win) {
+            std::cout << "Congratulations, you won the encounter!" << std::endl;
+            std::cout << "You found a " << item << "." << std::endl;
+        }
+        else {
+            std::cout << "Sorry, you lost the encounter." << std::endl;
+        }
+    }
 };
 
 int main()
